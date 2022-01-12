6bf059969937253eb9a97e3e63ae94943577dd39
Marci Vallario <marcivallario@gmail.com>
2022-01-12 12:05:55 -0500

    commit tree fd68ff747b2f266b874ebec5b3477c29bdc1d70f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize side1, side2, side3
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if greater_than_zero? && triangle_equality?
+      if @side1 == @side2 && @side2 == @side3 && @side3 == @side1
+        :equilateral
+      elsif @side1 == @side2 || @side2 == @side3 || @side3 == @side1
+        :isosceles
+      else 
+        :scalene
+      end
+    else 
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This is not a valid triangle."
+    end
+  end
+
+  private 
+
+  def greater_than_zero?
+    @side1 > 0 && @side2 > 0 && @side3 > 0
+  end
+
+  def triangle_equality?
+    @side1 + @side2 > @side3 && @side2 + @side3 > @side1 && @side3 + @side1 > @side2
+  end
 end
+
